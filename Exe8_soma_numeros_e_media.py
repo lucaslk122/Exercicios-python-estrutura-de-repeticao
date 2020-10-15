d2410b6b8a7dd01d00f4112916f7721d1ed4def5
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-15 13:33:28 -0300

    kkk

---
+++ Exe8_soma_numeros_e_media.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite um numero: "))
+num3 = float(input("Digite um numero: "))
+num4 = float(input("Digite um numero: "))
+num5 = float(input("Digite um numero: "))
+soma = num1 + num2 + num3 + num4 + num5
+media = soma / 5
+print(f"A soma dos 5 numeros é {soma} e a média é {round(media,2)}")
