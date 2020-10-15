0427141b1da0003deed661d897b86beaf7c6dd92
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-15 14:59:27 -0300

    modificado

--- Exe10_numeros_inteiros_compreendidos.py
+++ Exe10_numeros_inteiros_compreendidos.py
@@ -1,4 +1,4 @@
 n1 = int(input("Digite o primeiro numero: "))
 n2 = int(input("Digite o primeiro numero: "))
-for i in range(n1,n2):
+for i in range(n1+1,n2):
     print(i,end=" ")
---
+++ Exe11_exercicio10_retorna_soma.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o primeiro numero: "))
+for i in range(n1 + 1,n2):
+    print(i)
+for i in range(n2 + 1 ,n1):
+    print(i)
+print(f"Soma: ", i + i)
