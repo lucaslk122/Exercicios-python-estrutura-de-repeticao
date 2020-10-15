c5160eb331faabb369446f60f0313846ce7654f8
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-15 13:29:31 -0300

    kkk

--- Exe7_maior_numero.py
+++ Exe7_maior_numero.py
@@ -1,4 +1,6 @@
 num1 = float(input("Digite um numero: "))
 num2 = float(input("Digite um numero: "))
 num3 = float(input("Digite um numero: "))
-print(f"Dentro os numeros digitados o maior é {max(num1,num2,num3)}")
+num4 = float(input("Digite um numero: "))
+num5 = float(input("Digite um numero: "))
+print(f"Dentro os numeros digitados o maior é {max(num1,num2,num3,num4,num5)}")
