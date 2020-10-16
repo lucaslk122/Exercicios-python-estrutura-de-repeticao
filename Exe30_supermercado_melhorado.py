00ca1129813679fe9042abd3fc014d1fa36a2e00
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-16 11:44:29 -0300

    Create Exe30_supermercado_melhorado.py

---
+++ Exe30_supermercado_melhorado.py
@@ -0,0 +1,9 @@
+print("Bem vindo a panificadora do Sr Manoel Joaquim.")
+print("A quantidade de itens precia estar compreendida entre 1 e 50")
+qtd_itens = int(input("Qual a quantidade de paes?: "))
+preço_paes = float(input("Quanto custa o pão? "))
+while qtd_itens < 1 or  qtd_itens > 50:
+    print("Quantidade invalida")
+    qtd_itens = int(input("Qual a quantidade de itens?: "))
+totalApagar = qtd_itens * preço_paes
+print(f"Voce pagará R${round(totalApagar,2)} por {qtd_itens} itens")
