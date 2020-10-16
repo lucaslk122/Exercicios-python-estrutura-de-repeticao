66038e5c8f0b9e2b7ecb5eb501544d8c726da9ac
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-16 09:19:16 -0300

    Create Exe18_conjunto_numeros.py

---
+++ Exe18_conjunto_numeros.py
@@ -0,0 +1,11 @@
+num = int(input("Digite a quantidade de numeros que deseja no conjunto: "))
+pergunta = 0
+lista = []
+while pergunta < num:
+    num1 = int(input("Digite um numero: "))
+    pergunta += 1
+    lista.append(num1)
+print(f"O menor valor digitado foi {min(lista)}")
+print(f"O maior valor digitado foi {max(lista)}")
+print(f"A soma dos valores é {sum(lista)}")
+
