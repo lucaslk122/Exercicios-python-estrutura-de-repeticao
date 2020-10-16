5961a461059d0cc007653598e671618634182a2d
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-16 09:09:13 -0300

    Update Exe17_fatorial.py

--- Exe17_fatorial.py
+++ Exe17_fatorial.py
@@ -2,7 +2,7 @@ import math
 from math import factorial
 num = int(input("Digite um numero: "))
 #print(f"o fatorial de {num} é {math.factorial(num)}")
-factorial = 1
+fatorial = 1
 for i in range(1,num + 1):
-    factorial = factorial * i
-print(factorial)
+    fatorial = fatorial * i
+print(fatorial)
