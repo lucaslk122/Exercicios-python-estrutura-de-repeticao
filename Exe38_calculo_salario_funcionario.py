2686abb3fe0e9aa08e0c9a602abe246d774af6d5
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-19 11:57:23 -0300

    Update Exe38_calculo_salario_funcionario.py

--- Exe38_calculo_salario_funcionario.py
+++ Exe38_calculo_salario_funcionario.py
@@ -1,6 +1,17 @@
+"""
 salario = 1000 + (1000 * 0.015) #ganho em 1996
 ano = 1997
-while 1997 < 2020:
+while ano < 2020:
     salario = salario + (salario*(0.015*2))
     ano += 1
-print(salario)
+print(round(salario,2))
+"""
+
+#Modificado
+salario = float(input("Digite seu salario: "))
+ano = 1997
+salario = salario + (salario * 0.015)
+while ano < 2020:
+    salario = salario + (salario*(0.015*2))
+    ano += 1
+print(round(salario,2))
