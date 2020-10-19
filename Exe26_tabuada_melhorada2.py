083151dfdd74540ad5ac42fb060309810d95908e
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-19 08:26:42 -0300

    Update Exe26_tabuada_melhorada2.py

--- Exe26_tabuada_melhorada2.py
+++ Exe26_tabuada_melhorada2.py
@@ -1,6 +1,10 @@
 num = int(input("Montar a tabuada de: "))
 comeco = int(input("Começar por: "))
 final = int(input("Terminar em: "))
+while final < comeco:
+    print("o final precisa ser maior que o começo")
+    comeco = int(input("Começar por: "))
+    final = int(input("Terminar em: "))
 for i in range(comeco,final + 1):
     resul = num * i
     print(f"{num}x{i} = {resul}")
