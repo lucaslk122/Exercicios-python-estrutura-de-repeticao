9f8b5bc044802b07300b7f684d890793d98667e8
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-20 11:03:55 -0300

    Create Exe47_notas_candidatos.py

---
+++ Exe47_notas_candidatos.py
@@ -0,0 +1,19 @@
+nome = True
+nota = []
+while nome != "":
+    nome = input("Nome do participante: ")
+    if nome == "":
+        break
+    else:
+        for i in range(1,8):
+            x = float(input(f"{i}º nota: "))
+            nota.append(x)
+    print(f"Candidato: {nome}")
+    print(f"Melhor nota: {max(nota)}")
+    print(f"Pior nota: {min(nota)}")
+    nota.remove(min(nota))
+    nota.remove(max(nota))
+    media = sum(nota) / len(nota)
+    print(f"Média das notas: {round(media,2)}")
+    
+    
