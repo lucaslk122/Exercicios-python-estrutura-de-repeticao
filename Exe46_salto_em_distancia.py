4421c76739043b98092770e6b2b6e0f844391ed4
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-20 10:55:39 -0300

    Update Exe46_salto_em_distancia.py

--- Exe46_salto_em_distancia.py
+++ Exe46_salto_em_distancia.py
@@ -1,13 +1,18 @@
+nome = True
 salto = []
-nome = input("Nome do atletra: ")
-for i in range(1,6):
-    x = float(input(f"{i}º salto: "))
-    salto.append(x)
-print(f"Melhor salto: {max(salto)}")
-print(f"Pior salto: {min(salto)}")
-salto.remove(min(salto))
-salto.remove(max(salto))
-media = sum(salto) / 3
-print(f"Média dos saltos: {round(media,2)}m")
-print(f"""Resultado final:
-{nome} : {round(media,2)}m""")
+while nome != "":
+    nome = input("Nome do atletra: ")
+    if nome == "":
+        break
+    else:
+        for i in range(1,6):
+            x = float(input(f"{i}º salto: "))
+            salto.append(x)
+    print(f"Melhor salto: {max(salto)}")
+    print(f"Pior salto: {min(salto)}")
+    salto.remove(min(salto))
+    salto.remove(max(salto))
+    media = sum(salto) / 3
+    print(f"Média dos saltos: {round(media,2)}m")
+    print(f"""Resultado final:
+    {nome} : {round(media,2)}m""")
